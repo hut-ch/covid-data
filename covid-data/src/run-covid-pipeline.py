b85b4289022595e1f8081ad0dda0af45900238a6
Chris Hutton <cdmhutton@gmail.com>
2025-08-04 17:20:08 +0100

    updated config

--- covid-data/src/run-covid-pipeline.py
+++ covid-data/src/run-covid-pipeline.py
@@ -1,2 +1,5 @@
 # runs the main pipeling to extract codiv daat fro uk and eu sources if it doesnt already exist and tranfrom the data ready to be stored in final format
 
+from extract import covid_eu
+
+covid_eu()
